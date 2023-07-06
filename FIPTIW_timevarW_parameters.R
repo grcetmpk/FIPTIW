829c6a1b383144701430e632f591620148521127
Grace Tompkins <grace.tompkins97@gmail.com>
2023-07-06 10:13:12 -0400

    update to censoring sim to include option to make graphs

--- FIPTIW_timevarW_parameters.R
+++ FIPTIW_timevarW_parameters.R
@@ -33,6 +33,7 @@ nclusters <- makeCluster(ncores)
 
 require(knitr)
 require(kableExtra)
+require(ggplot2)
 
 
 #~~~~~~~~~~ FIPTIW simulation, assumptions satisfied (not shown in paper).
@@ -302,14 +303,14 @@ kable(varsel_n500$resultsmat_beta01, booktabs = T, digits = 2,format = "latex") 
 # tau = 7
 # N = 1000
 # censinform = F 
-# eta1vec = c(0, 0.1, 0.4)
+# eta1vec = c(0.4)
 # eta2vec = c(0, 0.2, 0.5)
 # eta3vec = c(0, 0.4, 0.6)
 # 
 # results_censoring_n100_ipcw <- simulateALLFIPTICW_CENS(N, n, beta1, beta2, beta3, gamma1,
-#                                                  gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = F,
+#                                                  gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = T,
 #                                                  censinform = T, eta1, eta2, eta3)
-saveRDS(results_censoring_n100_ipcw, "results_censoring_n100_ipcw.rds")
+# saveRDS(results_censoring_n100_ipcw, "results_censoring_n100_ipcw.rds")
 results_censoring_n100_ipcw <- readRDS("results_censoring_n100_ipcw.rds")
 
 kable(results_censoring_n100_ipcw$resultsmat[, c(1,2,3, 4,6,7,9,10,12, 13, 15, 16, 18)], booktabs = T, 
@@ -333,14 +334,14 @@ kable(results_censoring_n100_ipcw$resultsmat[, c(1,2,3, 4,6,7,9,10,12, 13, 15, 1
 # tau = 7
 # N = 1000
 # censinform = F
-# eta1vec = c(0, 0.1, 0.4)
+# eta1vec = c(0.4)
 # eta2vec = c(0, 0.2, 0.5)
 # eta3vec = c(0, 0.4, 0.6)
 # 
 # results_censoring_n50_ipcw <- simulateALLFIPTICW_CENS(N, n, beta1, beta2, beta3, gamma1,
-#                                                  gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = F,
+#                                                  gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = T,
 #                                                  censinform = T, eta1, eta2, eta3)
-saveRDS(results_censoring_n50_ipcw, "results_censoring_n50_ipcw.rds")
+# saveRDS(results_censoring_n50_ipcw, "results_censoring_n50_ipcw.rds")
 results_censoring_n50_ipcw <- readRDS("results_censoring_n50_ipcw.rds")
 
 kable(results_censoring_n50_ipcw$resultsmat[, c(1,2,3, 4,6,7,9,10,12, 13, 15, 16, 18)], booktabs = T, 
@@ -364,17 +365,17 @@ alpha1 = 1
 tau = 7
 N = 1000
 censinform = F
-eta1vec = c(0, 0.1, 0.4)
+eta1vec = c(0.4)
 eta2vec = c(0, 0.2, 0.5)
 eta3vec = c(0, 0.4, 0.6)
 
-results_censoring_n500_ipcw <- simulateALLFIPTICW_CENS(N, n, beta1, beta2, beta3, gamma1,
-                                                      gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = F,
-                                                      censinform = T, eta1, eta2, eta3)
-saveRDS(results_censoring_n500_ipcw, "results_censoring_n500_ipcw.rds")
+# results_censoring_n500_ipcw <- simulateALLFIPTICW_CENS(N, n, beta1, beta2, beta3, gamma1,
+#                                                       gamma2vec, gamma3vec, alpha0, alpha1, tau, outputfulldatalist = T,
+#                                                       censinform = T, eta1, eta2, eta3)
+# saveRDS(results_censoring_n500_ipcw, "results_censoring_n500_ipcw.rds")
 results_censoring_n500_ipcw <- readRDS("results_censoring_n50 fdswjhmv c0_ipcw.rds")
 
-kable(results_censoring_n50_ipcw$resultsmat[, c(1,2,3, 4,6,7,9,10,12, 13, 15, 16, 18)], booktabs = T, 
+kable(results_censoring_n500_ipcw$resultsmat[, c(1,2,3, 4,6,7,9,10,12, 13, 15, 16, 18)], booktabs = T, 
       digits = 3,format = "latex") %>%
   add_header_above(c(" " = 3, "Unweighted" = 2, "IIW" = 2, "IPTW" = 2, "FIPTIW" = 2, "FIPTICW" = 2))
 
